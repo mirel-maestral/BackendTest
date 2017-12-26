de283bfcbbc8d65d528b7ceeac907074f8d16e46
Mirel <mirelsp@maestralsolutions.com>
2017-12-27 00:10:47 +0100

    Adding post deployment script

---
+++ Database1/Script.PostDeployment1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+--INSERTING ORDERS FOR TESTING
+DECLARE @cnt INT = 0;
+
+WHILE @cnt < 300000
+BEGIN
+   INSERT INTO Orders(Processed, Locked)
+   VALUES(0, 0)
+   SET @cnt = @cnt + 1;
+END;
